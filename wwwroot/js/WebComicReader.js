f5d1fc58fa40a02ff77307628919a2a42a5fe893
Marcus André <ddtank.marcus@gmail.com>
2020-08-13 18:22:17 -0300

    Simple Zoom

--- wwwroot/js/WebComicReader.js
+++ wwwroot/js/WebComicReader.js
@@ -13,6 +13,9 @@
         gradients: true,
         autoCenter: true
     });
+
+    PostLoad(false);
+
     return $(window).width() / 2;
 }
 
@@ -31,9 +34,147 @@ async function LoadManga() {
         gradients: true,
         autoCenter: true
     });
+
+    PostLoad(true);
+
     return $(window).width() / 2;
 }
 
+function PostLoad(swap) {
+    $('.flipbook-viewport').zoom({
+        flipbook: $('.flipbook'),
+
+        max: function () {
+
+            return largeWidth() / $('.flipbook').width();
+
+        },
+
+        when: {
+            swipeLeft: function () {
+
+                $(this).zoom('flipbook').turn('next');
+
+            },
+
+            swipeRight: function () {
+
+                $(this).zoom('flipbook').turn('previous');
+
+            },
+
+            zoomIn: function () {
+
+                $('#slider-bar').hide();
+                $('.made').hide();
+                $('.flipbook').removeClass('animated').addClass('zoom-in');
+
+                if (!window.escTip && !$.isTouch) {
+                    escTip = true;
+
+                    $('<div />', { 'class': 'exit-message' }).
+                        html('<div>Press ESC to exit</div>').
+                        appendTo($('body')).
+                        delay(2000).
+                        animate({ opacity: 0 }, 500, function () {
+                            $(this).remove();
+                        });
+                }
+            },
+
+            zoomOut: function () {
+
+                $('#slider-bar').fadeIn();
+                $('.exit-message').hide();
+                $('.made').fadeIn();
+
+                setTimeout(function () {
+                    $('.flipbook').addClass('animated').removeClass('zoom-in');
+                    resizeViewport();
+                }, 0);
+
+            }
+        }
+    });
+
+    if ($.isTouch)
+        $('.flipbook-viewport').bind('zoom.doubleTap', zoomTo);
+    else
+        $('.flipbook-viewport').bind('zoom.tap', zoomTo);
+
+    $(document).keydown(function (e) {
+        var previous = 37, next = 39, esc = 27;
+
+        if (swap)
+            previous = 39, next = 37;
+
+        switch (e.keyCode) {
+            case previous:
+                $('.flipbook').turn('previous');
+                e.preventDefault();
+                break;
+            case next:
+                $('.flipbook').turn('next');
+                e.preventDefault();
+                break;
+            case esc:
+                $('.flipbook-viewport').zoom('zoomOut');
+                e.preventDefault();
+                break;
+        }
+    });
+    $('.flipbook-viewport')[0].style.position = "unset";
+}
+
+function zoomTo(event) {
+
+    setTimeout(function () {
+        if ($('.flipbook-viewport').zoom('value') == 1) {
+            $('.flipbook-viewport').zoom('zoomIn', event);
+        } else {
+            $('.flipbook-viewport').zoom('zoomOut');
+        }
+    }, 1);
+
+}
+
+function resizeViewport() {
+
+    var width = $(window).width(),
+        height = $(window).height(),
+        options = $('.flipbook').turn('options');
+
+    $('.flipbook').removeClass('animated');
+
+    $('.flipbook-viewport').css({
+        width: width,
+        height: height
+    }).zoom('resize');
+
+    var flipbookOffset = $('.flipbook').offset(),
+        boundH = height - flipbookOffset.top - $('.flipbook').height(),
+        marginTop = (boundH - $('.thumbnails > div').height()) / 2;
+
+    if (marginTop < 0) {
+        $('.thumbnails').css({ height: 1 });
+    } else {
+        $('.thumbnails').css({ height: boundH });
+        $('.thumbnails > div').css({ marginTop: marginTop });
+    }
+
+    if (flipbookOffset.top < $('.made').height())
+        $('.made').hide();
+    else
+        $('.made').show();
+
+    $('.flipbook').addClass('animated');
+
+}
+
+function largeWidth() {
+    var width = $(window).width();
+    return width + (width * 0.50);//50% of zoom
+}
 
 async function OpenFile() {
     comic.click();
